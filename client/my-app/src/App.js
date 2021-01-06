d1333af76d2852dcd551a030a3f91e4b58cf37c6
fdh.luis.angel <fdh.luis.angel@gmail.com>
2021-01-06 12:45:11 -0500

    Agregando rutas en react

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,14 +1,34 @@
 import LoginPage from './views/LoginPage/LoginPage';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <Provider store={store}>
-        <LoginPage/>
-      </Provider>
-    </div>
+    <Router>
+      <div className="App">
+        <Provider store={store}>
+          <Switch>
+            <Route path="/login">
+              <LoginPage/>
+            </Route>
+            <Route path="/admin">
+              <div>Admin</div>
+            </Route>
+            <Route path="/">
+              <div>Home</div>
+            </Route>
+          </Switch>
+          
+        </Provider>
+      </div>
+    </Router>
+    
   );
 }
 
